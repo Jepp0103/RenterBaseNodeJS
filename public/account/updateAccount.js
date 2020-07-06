71f42751c32ff8a3357b6af5e7eb019a9a086bed
Jepp0103 <jeppendyekjaer@hotmail.com>
2020-07-06 17:59:50 +0200

    ..

--- public/account/updateAccount.js
+++ public/account/updateAccount.js
@@ -1,6 +1,6 @@
 // Ajax call for getting the username
 $.get("/updateUserData").done(data => {
-    $("#username2").val(data.response.username);
+    $("#username1").val(data.response.username);
     $("#email").val(data.response.email);
 });
 
--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const fileSystem = require("fs");
 const bcrypt = require("bcrypt");
+const User = require("../models/User");
+const { rawListeners } = require("process");
 //const User ...
 
 router.get("/", (req, res) => {
@@ -14,13 +16,55 @@ router.get("/", (req, res) => {
 //GET methods
 router.get("/login", (req, res) => {
     if (!req.session.login) {
-        const loginPage = fileSystem.readFileSync("./public/login/login.html", "utf8");
+        const loginPage = fileSystem.readFileSync("./public/login/login.html", "utf-8");
         return res.send(loginPage);
     } else {
         return res.redirect("/main");
     }
 });
 
-r
+router.get("/main", (req, res) => {
+    if (req.session.login) {
+        const navbarPage = fileSystem.readFileSync("./public/navbar/navbar.html", "utf-8");
+        const mainPage = fileSystem.readFileSync("./public/main/main.html", "utf-8");
+        const footerPage = fileSystem.readFileSync("./public/footer/footer.html", "utf-8");
+        return res.send(navbarPage + mainPage + footerPage);
+    } else {
+        return res.redirect("login");
+    }
+});
+
+//POST methods
+router.post("/main", async (req, res) => {
+    const { username, password } = req.body;
+    try {
+        if (username && password) {
+            const validPassword = await User.query().select("id", "username", "email", "password").where("username", username);
+        }
+
+        if (validPassword.length !== 1) { //Checking if one matching user appears from the database.
+            return res.redirect("login");
+        }
+
+        if (validPassword.length === 1) {
+            bcrypt.compare(password, validPassword[0].password).then(compare => {
+                if (compare === true) {
+                    req.session.userId = validPassword[0].id;
+                    req.session.login = true;
+                    req.session.username = username;
+                    req.session.email = validPassword[0].email;
+                    return res.redirect("/main");
+                } else {
+                    return res.redirect("login");
+                }
+            });
+        } else {
+            return res.redirect("login");
+        }
+    } catch (error) {
+        console.log(error);
+    }
+});
+
 
 module.exports = router;
